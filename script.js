06c497102187dc17973b1c07658453904e1dca91
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2024-03-07 18:17:22 +0500

    Login page 1 (js)

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
-// var a= document.querySelector("#box1");
-// a.style.backgroundColor="red";
+let signup = document.querySelector('.signup');
+let signin = document.querySelector('.signin');
+let namefield = document.querySelector('.namefield');
+let title = document.querySelector('.title');
+let underline = document.querySelector('.underline');
 
-// a.textContent="hey bro";
-// a.innerHTML="<h1>hey bro</h1>";
 
-// a.addEventListener('mouseenter', function(){
-//     a.style.backgroundColor="blue";
-// })
-// a.addEventListener('mouseleave', function(){
-//     a.style.backgroundColor="red";
-// })
+signin.addEventListener('click', () => {
+    namefield.style.display = 'none';
+    title.innerHTML = 'Sign In';
+    // underline.style.width = '60px';
+    signup.classList.add('disable');
+    signin.classList.remove('disable');
+    underline.style.transform = 'translateX(35px)';
 
-var arr=[
-    {name:"sandeep", image:"https://images.unsplash.com/photo-1682695798522-6e208131916d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"},
-    {name:"sand", image:"https://images.unsplash.com/photo-1709632237343-4583d7f96f52?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {name:"deep", image:"https://images.unsplash.com/photo-1709710189876-8aebd66de2f7?q=80&w=1167&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {name:"san", image:"https://images.unsplash.com/photo-1709532696583-b19c20ed2894?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {name:"eep", image:"https://images.unsplash.com/photo-1707343843437-caacff5cfa74?q=80&w=1075&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+});
+signup.addEventListener('click', () => {
+    namefield.style.display = 'flex';
+    title.innerHTML = 'Sign Up';
+    // underline.style.width = '60px';
+    signup.classList.remove('disable');
+    signin.classList.add('disable');
+    underline.style.transform = 'translateX(0)';
 
-]
-
-function showthecards(){
-    var clutter ="";
-    arr.forEach(function(obj){
-        clutter+= `<div class="box">
-        <img class="cursor-pointer" src="${obj.image}" alt="">
-    </div>`;
-
-    })
-    document.querySelector(".container").innerHTML=clutter;
-}
-showthecards();
+});
